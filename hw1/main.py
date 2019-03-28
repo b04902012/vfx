77b78532e601e812c01ec3dfd75b91fae52c0317
albert0liu0 <b04902012@ntu.edu.tw>
2019-03-28 16:05:38 +0800

    hw1

---
+++ hw1/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import myhdr as hdr
+import os.path
+import pickle
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        dir_name = input('Please input image name: ')
+    else:
+        dir_name = sys.argv[1]
+
+    if input('Already has radiance map? [y/n] ') in ('y', 'Y'):
+        rad_img = hdr.loadHDR(dir_name+'/'+dir_name+'_rad')
+    else:
+        rad_img = hdr.computeHDR(dir_name)
+
+    if input('Do tone mapping? [y/n] ') in ('y', 'Y'):
+        
+        print(np.amax(rad_img))
+        print(np.amin(rad_img))
+
+        key_value, multi_value = input('Please input key value and multi value: ').split()
+        key_value, multi_value = float(key_value), float(multi_value)
+        tone_mapped = hdr.photographic(rad_img, key_value, multi_value)
+
+        print(np.amax(tone_mapped))
+        print(np.amin(tone_mapped))
+
+        cv2.imwrite(dir_name+'/'+dir_name+f'_toned_{key_value}_{multi_value}.png',tone_mapped)
+        print(f'Save image to {dir_name}/{dir_name}_toned_{key_value}_{multi_value}.png')
+        
---
+++ hw1/myhdr.py
@@ -0,0 +1,262 @@
+import numpy as np
+import cv2
+import random
+import pickle
+from matplotlib import pyplot as plt
+
+
+### I/O ###
+def readImages(dir_name, txt_name):
+    print("Reading images...", end="        ")
+    with open(dir_name + txt_name, 'r') as f:
+        for line in f:
+            if line[0] == '#':
+                continue
+            num_img = int(line)
+            break
+
+        contents = []
+        for line in f:
+            if line[0] == '#':
+                continue
+            contents.append(line.split())
+
+        filenames = [content[0] for content in contents]
+        log_t = list(map(float, [content[1] for content in contents]))
+        log_t = -np.log2(log_t)
+        [print("-", f) for f in filenames]
+
+    # grayscale -> 0
+    # RGB -> >0
+    imgs = []
+    for f in filenames:
+        raw_image = cv2.imread(dir_name+f.replace("ppm", "png"), 1)
+        r_rate = round(max(raw_image.shape)/1000)
+        resized_image = cv2.resize(raw_image, (0, 0), fx=1/r_rate, fy=1/r_rate, interpolation=cv2.INTER_AREA)
+        imgs.append(resized_image)
+    #   printFigure(img[-1])
+
+    print("done.\n")
+    return imgs, log_t
+
+def loadHDR(filename):
+    with open(filename, 'rb') as f:
+        rad_img = pickle.load(f)
+    return rad_img
+
+def writeHDRFile(image, filename):
+    with open(filename+".hdr", "wb") as f:
+        f.write("#?RADIANCE\nFORMAT=32-bit_rle_rgbe\n\n".encode())
+        f.write(f"-Y {image.shape[0]} +X {image.shape[1]}\n".encode())
+
+        brightest = np.maximum(np.maximum(image[...,0], image[...,1]), image[...,2])
+        mantissa = np.zeros_like(brightest)
+        exponent = np.zeros_like(brightest)
+        np.frexp(brightest, mantissa, exponent)
+        scaled_mantissa = mantissa * 256.0 / brightest
+        rgbe = np.zeros((image.shape[0], image.shape[1], 4), dtype=np.uint8)
+        rgbe[...,0:3] = np.around(image[...,0:3] * scaled_mantissa[...,None])
+        rgbe[...,3] = np.around(exponent + 128)
+
+        rgbe.ravel().tofile(f)
+    return rgbe
+
+def printFigure(img):
+    for r in range(0, img.shape[0], 10):
+        for c in range(0, img.shape[1], 20):
+            print(img[r, c:c+5])
+
+    print("max =", np.amax(img))
+    print("min =", np.amin(img))
+
+    cv2.imwrite('image.png',img)
+    cv2.imshow('image',img)
+    k = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+### HDR ###
+def weight(pixel):
+    z_min, z_max = 0, 255
+    if pixel <= (z_min+z_max) / 2:
+        return pixel - z_min
+    return z_max - pixel
+
+
+def getSamples(images, z_min = 0, z_max = 255):
+    z_range = z_max - z_min + 1
+    nImages = len(images)
+    nSamples = z_range
+    intensity_values = np.zeros((nSamples, nImages), dtype=np.uint8)
+
+    '''
+    rows = np.random.randint(images[0].shape[0], size=(nSamples))
+    cols = np.random.randint(images[0].shape[1], size=(nSamples))
+    
+    for i in range(len(rows)):
+        for j in range(nImages):
+            intensity_values[i, j] = images[j][rows[i], cols[i]]
+
+    return intensity_values
+    
+    '''
+    mid_img = images[nImages // 2]
+
+    for i in range(z_range):
+        rows, cols = np.where(mid_img == i)
+        if len(rows) != 0:
+            idx = random.randrange(len(rows))
+            for j in range(nImages):
+                intensity_values[i, j] = images[j][rows[idx], cols[idx]]
+    return intensity_values
+    
+
+
+def getResponseCurve(Z, log_t, smooth_lambda, weighting_func, z_min = 0, z_max = 255):
+    n = z_max - z_min + 1
+
+    nSamples = Z.shape[0]
+    nImages = len(log_t)
+
+    A = np.zeros((nSamples * nImages + n + 1, n + nSamples))
+    B = np.zeros((A.shape[0], 1))
+
+
+    # Include the data-fitting equations
+
+#    print(Z)
+    k = 0
+    for i in range(nSamples):
+        for j in range(nImages):
+            w_ij = weighting_func(Z[i, j]+1)
+            A[k, Z[i, j]+1] = w_ij
+            A[k, n+i] = -w_ij
+            B[k, 0] = w_ij * log_t[j]
+            k += 1
+
+    A[k, n//2] = 1
+    k += 1
+
+    for i in range(n-2):
+        A[k, i] = smooth_lambda * weighting_func(i+1)
+        A[k, i+1] = -2 * smooth_lambda * weighting_func(i+1)
+        A[k, i+2] = smooth_lambda * weighting_func(i+1)
+        k += 1
+
+    invA = np.linalg.pinv(A)
+    x = np.dot(invA, B)
+
+    g = x[0:n]
+    print(x[n//2])
+    return g[:, 0]
+
+
+def getRadianceMap(images, log_t, response_curve, weighting_func):
+    print("Computing radiance map......", end="         ")
+    rows, cols = images[0].shape
+    rad_img = np.zeros((rows, cols))
+    nImages = len(images)
+
+    for r in range(rows):
+        for c in range(cols):
+            log_e = 0
+            weight = np.array([weighting_func(images[i][r, c]) for i in range(nImages)])
+            weight_sum = np.sum(weight)
+            log_e = np.array([response_curve[images[i][r, c]] - log_t[i] for i in range(nImages)])
+            weighted_e = np.dot(weight, log_e)
+
+            if weight_sum != 0:
+                weighted_e /= weight_sum
+            else:
+                weighted_e = response_curve[images[nImages//2][r, c]] - log_t[nImages//2]
+            rad_img[r, c] = 2 ** weighted_e
+#            print(log_e)
+#            rad_img[r, c] = log_e
+    print("done.")
+    return rad_img
+
+def computeHDR(dir_name):
+    imgs, log_t = readImages(dir_name+'/', '.hdr_image_list.txt')
+    channels = [[img[:, :, i] for img in imgs] for i in range(3)]
+    rad_img = np.zeros(imgs[0].shape)    
+    print("image shape:", imgs[0].shape)
+
+    color = ['red', 'green', 'blue']
+    responseCurve = []
+    for i in range(3):
+        print(color[i])
+        samples = getSamples(channels[i])
+#       print(log_t)
+        responseCurve.append(getResponseCurve(samples, log_t, 100, weight))
+#       print(responseCurve[i])
+        plt.plot(responseCurve[i], np.arange(256), c=color[i])
+        plt.savefig(f'{dir_name}/curve_{i+1}.png')
+        plt.clf()
+#       plt.show(block='false')
+        rad_img[..., i] = getRadianceMap(channels[i], log_t, responseCurve[i], weight)
+        print(rad_img[:, :, i])
+        print("max =", np.amax(rad_img[:, :, i]))
+        print("min =", np.amin(rad_img[:, :, i]))
+    '''
+    print(rad_img.shape)
+    '''
+    for i in range(3):
+        plt.plot(responseCurve[i], np.arange(256), c=color[i])
+    plt.savefig(f'{dir_name}/curve_{4}.png')
+    plt.clf()
+    
+    with open(dir_name+'/'+dir_name+'_rad', 'wb') as f:
+        pickle.dump(rad_img, f)
+
+    writeHDRFile(rad_img, dir_name+'/'+dir_name)
+    
+
+    '''
+    for r in range(0, 768, 10):
+        for c in range(0, 502, 20):
+            print(rad_img[r, c:c+5])
+
+    for r in range(0, 768, 10):
+        for c in range(0, 502, 20):
+            print(rad_img[r, c:c+5])
+    '''
+    
+    cv2.imwrite(dir_name+'/'+dir_name+'.png',rad_img)
+#    cv2.imshow('image',rad_img)
+#    k = cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return rad_img
+    
+
+
+### tone mapping ###
+def photographic(rad_img, key_value = 0.18, multi_value = 1):
+    '''
+    rad_img: (rowls, cols, 3)
+    '''
+    delta   = 0.001
+
+    Lw      = 0.27*rad_img[:, :, 0] + 0.67*rad_img[:, :, 1] + 0.06*rad_img[:, :, 2]
+    #Lw = 0.3*rad_img[:, :, 0] + 0.59*rad_img[:, :, 1] + 0.11*rad_img[:, :, 2]
+    L_w  = np.log2(delta + Lw)
+
+    L_w     = np.exp2(np.sum(L_w)/rad_img.shape[0]/rad_img.shape[1], out=L_w)
+    print(L_w)
+    L_m     = key_value / L_w * Lw
+    L_white = np.amax(L_m)
+    L_d     = L_m * (1 + L_m/(L_white ** 2)) / (1 + L_m)
+    print(L_d)
+    print(np.amax(L_d))
+    print(np.amin(L_d))
+
+    tone_mapped_img = rad_img * L_w
+    channels = [np.expand_dims(tone_mapped_img[:, :, i]/L_d, 2) for i in range(3)]
+
+    tone_mapped_img = np.concatenate(channels, 2)
+
+    # adjust intensity
+    tone_mapped_img *= 128 / np.average(tone_mapped_img) * multi_value
+
+#    printFigure(tone_mapped_img)
+
+    return tone_mapped_img
