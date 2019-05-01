fix Harris corner detector by using cv2.Sobel() instead of np.gradient

hw2/main.py
@@ -26,60 +26,73 @@ def readImages(dir_name):
 			full_name = os.path.join(dir_name, image_name.strip())
 			print('  -', full_name)
 			imgs.append(cv2.imread(full_name))
-			'''
-			if imgs[-1] == None:
-				print("Can't find", full_name, "!")
-			'''
 
 	return imgs
 
-def featureDetection(color_imgs, imgs):
-	# parameters
-	window_size = 5
-	k = 0.05
-	threshold = 13000000000
-
+def featureDetection(color_imgs, imgs, window_size=3, k=0.05, threshold=None):
+	"""
+	
+	Returns:
+		list of list of tuples (coordinates of the detected keypoints in each image)
+	"""
 	offset = (window_size-1)//2
 	sigma = (window_size+1)/3
 
-	cornerlist = []
+	cornerlist = [[] for img in imgs]
 
-	x, y = np.mgrid[-2:3, -2:3]
+	x, y = np.mgrid[-offset:offset+1, -offset:offset+1]
 	gaussian = np.exp(-(x**2+y**2)/2/sigma**2)
 
 	g = lambda window: (window*gaussian).sum()
 
-	for i, (color_img, img) in enumerate(zip(color_imgs, imgs)):
-		h, w = img.shape
+	with open("log", "w") as f:
+		for i, (color_img, img) in enumerate(zip(color_imgs, imgs)):
 
-		Ix, Iy = np.gradient(img)
-		Ixx = Ix**2
-		Iyy = Iy**2
-		Ixy = Ix*Iy
+			h, w = img.shape
 
-		for x in range(offset, h-offset):
-			#print(x)
-			for y in range(offset, w-offset):
-				M = np.array(	((g(Ixx[x-offset:x+offset+1, y-offset:y+offset+1]),
-								 g(Ixy[x-offset:x+offset+1, y-offset:y+offset+1])),
-								(g(Ixy[x-offset:x+offset+1, y-offset:y+offset+1]),
-								 g(Iyy[x-offset:x+offset+1, y-offset:y+offset+1]))))
-				#print(M)
-				
-				eigs = LA.eigvals(M)
+			#Ix, Iy = np.gradient(img)		# buggy gradient
+			Ix = cv2.Sobel(img, cv2.CV_16S, 1, 0)
+			Iy = cv2.Sobel(img, cv2.CV_16S, 0, 1)
 
-				R = eigs[0]*eigs[1] - k*((eigs[0]+eigs[1])**2)
-				if R > threshold:
-					cornerlist.append((x, y))
-					color_img.itemset((x, y, 0), 0)
-					color_img.itemset((x, y, 1), 0)
-					color_img.itemset((x, y, 2), 255)
-		
-		cv2.imwrite(f"final{i}.png", color_img)
+			Ixx = Ix**2
+			Iyy = Iy**2
+			Ixy = Ix*Iy
 
-		# temporary hack: only produce 3 photos
-		if i == 2:
-			return
+			for x in range(2*offset, h-2*offset):
+				for y in range(2*offset, w-2*offset):
+					M = np.array(	((g(Ixx[x-offset:x+offset+1, y-offset:y+offset+1]),
+									 g(Ixy[x-offset:x+offset+1, y-offset:y+offset+1])),
+									(g(Ixy[x-offset:x+offset+1, y-offset:y+offset+1]),
+									 g(Iyy[x-offset:x+offset+1, y-offset:y+offset+1]))))
+					
+					eigs = LA.eigvals(M)
+
+					R = eigs[0]*eigs[1] - k*((eigs[0]+eigs[1])**2)
+
+					f.write(str(R)+'\n')
+					
+					if threshold == None:
+						cornerlist[i].append((R, (x, y)))
+
+					elif R > threshold:
+						cornerlist[i].append((x, y))
+						
+			if threshold == None:
+				cornerlist[i].sort()
+				#for j in range(5000):
+				#	print(cornerlist[-i-1][0])
+				cornerlist[i] = [(x, y) for r, (x, y) in cornerlist[i][-5000:]]
+
+			for x, y in cornerlist[i]:
+				color_img.itemset((x, y, 0), 0)
+				color_img.itemset((x, y, 1), 0)
+				color_img.itemset((x, y, 2), 255)
+			
+			cv2.imwrite(f"final{i}.png", color_img)
+
+
+	#print(cornerlist)
+	return cornerlist
 
 
 