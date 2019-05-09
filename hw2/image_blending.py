cfa2fa3878bd00f52cb9b86af52c735d595a5713
heyhummingbird <setfreeyang@gmail.com>
2019-05-09 22:21:53 +0800

    blending

--- hw2/image_blending.py
+++ hw2/image_blending.py
@@ -1,6 +1,9 @@
+import numpy as np
+import cv2
+
 def transform(transform_matrix, coordinate):
     new_coordinate = np.matmul(transform_matrix,np.transpose(np.array([coordinate[0],coordinate[1],1])))
-    return np.array([new_coordinate[0]/new_coordinate[2],new_coordinate[1]/new_coordinate[2]])
+    return np.array([int(new_coordinate[0]/new_coordinate[2]),int(new_coordinate[1]/new_coordinate[2])])
 
 def image_blending(images, transforms):
     h, w, channels = images[0].shape
@@ -10,15 +13,15 @@ def image_blending(images, transforms):
     xi = 100
     yi = 10
     
-    cur_transform = transforms[0]
     outputImage[xi:xi+h, yi:yi+w] = images[0]
     last_y = yi+w
 
     for i in range(1, len(images)):
-        cur_transform = np.matmul(cur_transform, transforms[i])
-        img = np.transpose(cv2.warpPerspective(src = np.transpose(images[i]), M = cur_transform, dsize = (all_h, all_w)))
-        (x1, y1), (x2, y2) = transform(cur_transform, [0, 0]), transform(cur_transform, [h, 0])
-        (x3, y3), (x4, y4) = transform(cur_transform, [0, w]), transform(cur_transform, [h, w])
+        img = np.zeros((all_h, all_w, 3))
+        for channel in range(3):
+            img[:, :, channel] = np.transpose(cv2.warpPerspective(src = np.transpose(images[i][:, :, channel]), M = transforms[i], dsize = (all_h, all_w)))
+        (x1, y1), (x2, y2) = transform(transforms[i], [0, 0]), transform(transforms[i], [h, 0])
+        (x3, y3), (x4, y4) = transform(transforms[i], [0, w]), transform(transforms[i], [h, w])
 
         this_y = max(y1, y2)
         linear_width = last_y - this_y
@@ -26,9 +29,12 @@ def image_blending(images, transforms):
         for y in range(this_y, last_y):
             alpha = (y-this_y)/linear_width
             for x in range(all_h):
-                if np.any(img[x, y]) and np.any(outputImage[x, y]):
-                    outputImage[x, y] = outputImage[x, y]*(1-alpha) + img[x, y]*alpha
-        outputImage[xi+max(x1, x3):xi+min(x2, x4), yi+last:yi+max(y3, y4)] = img[max(x1, x3):min(x2, x4), last:max(y3, y4)]
+                if np.any(img[x, y]) and np.any(outputImage[x+xi, y+yi]):
+                    outputImage[x+xi, y+yi] = outputImage[x+xi, y+yi]*(1-alpha) + img[x, y]*alpha
+
+        print(xi+max(x1, x3), xi+min(x2, x4), yi+last_y, yi+max(y3, y4))
+        print(max(x1, x3), min(x2, x4), last_y, max(y3, y4))
+        outputImage[xi+max(x1, x3, 0):xi+min(x2, x4, h), yi+last_y:yi+max(y3, y4)] = img[max(x1, x3, 0):min(x2, x4, h), last_y:max(y3, y4)]
         last_y = min(y3, y4)
 
     return outputImage
